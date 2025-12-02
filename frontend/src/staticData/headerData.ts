025536b1531b3ba0ff8309f7fd52ded192f90504
johnson-oragui <johnson.oragui@gmail.com>
2025-12-02 14:11:05 +0100

    feat: added footer static data

---
+++ frontend/src/staticData/headerData.ts
@@ -0,0 +1,8 @@
+const navLinks = [
+	{ name: 'Dashboard', href: '/dashboard' },
+	{ name: 'Team', href: '/team' },
+	{ name: 'Projects', href: '/projects' },
+	{ name: 'Reports', href: '/reports' },
+];
+
+export { navLinks };
