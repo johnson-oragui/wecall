2af63e2eb7456f0eaca0fb7865174a3d1a329c09
johnson-oragui <johnson.oragui@gmail.com>
2025-12-07 12:20:17 +0100

    feat: added limited countryCodes

---
+++ frontend/src/staticData/signupData.ts
@@ -0,0 +1,13 @@
+const countryCodes = [
+	{ code: '+1', name: 'USA/Canada' },
+	{ code: '+44', name: 'UK' },
+	{ code: '+91', name: 'India' },
+	{ code: '+49', name: 'Germany' },
+	{ code: '+61', name: 'Australia' },
+	{ code: '+33', name: 'France' },
+	{ code: '+81', name: 'Japan' },
+	{ code: '+86', name: 'China' },
+	{ code: '+234', name: 'Nigeria' },
+];
+
+export { countryCodes };
