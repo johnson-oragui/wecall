4e20b036d5f820275cd0f71b789fe9279c82c888
johnson-oragui <johnson.oragui@gmail.com>
2025-12-21 14:25:35 +0100

    feat: add notify

---
+++ frontend/src/app/api/v1/ws/notify/route.ts
@@ -0,0 +1,30 @@
+import { cookies } from 'next/headers';
+import WebSocket from 'ws';
+
+let ws: WebSocket | null = null;
+
+export async function POST() {
+	const token = (await cookies()).get('access_token')?.value;
+
+	if (!token) return new Response('Unauthorized', { status: 401 });
+
+	if (ws && ws.readyState === WebSocket.OPEN)
+		return new Response('Already connected');
+
+	ws = new WebSocket(`${process.env.BACKEND_WS_URL}/ws/notify/?token=${token}`);
+
+	ws.on('open', (stream: unknown) => {
+		console.log('Notify WS connected');
+		console.log('stream: ', stream);
+	});
+
+	ws.on('message', (data) => {
+		console.log('WebSocket.RawData: ', data);
+	});
+
+	ws.on('close', () => {
+		ws = null;
+	});
+
+	return new Response('Ws connected');
+}
